d11c8a41e20ab4374f94bc28e05f3ac5f7387a45
Jarod <jarod.hargreaves@gmail.com>
2018-08-19 22:54:55 +0100

    Initial commit with Vue CLI

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,16 @@
+<template>
+	<div class="">
+		First component
+	</div>
+</template>
+
+<script>
+export default {
+	name: 'HelloWorld',
+	props: {
+	},
+};
+</script>
+
+<style scoped lang="scss">
+</style>
